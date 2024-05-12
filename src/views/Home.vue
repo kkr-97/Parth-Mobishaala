712bf96051a0d491ebfa296400be93c221c841e7
kkr-97 <112936374+kkr-97@users.noreply.github.com>
2024-05-12 19:03:29 +0530

    Update Home.vue
    change video

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -56,7 +56,7 @@
             "
           >
             <iframe
-              src="https://www.youtube.com/embed/HmMaH3QZJvQ"
+              src="https://www.youtube.com/embed/kB3dyksaBx0"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
